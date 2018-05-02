a52166d893778d9088a9ca7530ab839794a978fd
psantosiii <psantosiii@yahoo.com>
2018-05-02 11:00:28 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,100 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# print (__name__)
+# @app.route('/')
+
+# def hello_world():
+#     return 'Hello world'
+# if __name__=="__main__":
+    
+#     app.run(debug=True)
+
+#DOJO
+# from flask import Flask
+# app = Flask(__name__)
+
+# print (__name__)
+# @app.route('/')
+
+# def dojo():
+#     return "Dojo"
+# if __name__=="__main__":
+
+#     app.run(debug=True)
+#Hi Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+# print (__name__)
+# @app.route('/')
+
+# def flask():
+#     return "Hi Flask"
+# if __name__=="__main__":
+    
+#     app.run(debug=True)
+
+# #hi Michael
+# from flask import Flask
+# app = Flask(__name__)
+
+# print (__name__)
+# @app.route('/')
+
+# def flask():
+#     return "HI Michael"
+# if __name__=="__main__":
+    
+#     app.run(debug=True)
+
+# #Hi John
+# from flask import Flask
+# app = Flask(__name__)
+
+# print (__name__)
+# @app.route('/')
+
+# def flask():
+#     return "Hi John"
+# if __name__=="__main__":
+#     print("Hi John")
+    
+#     app.run(debug=True)
+
+# #35 Hello
+# from flask import Flask
+# app = Flask(__name__)
+
+# print(__name__)
+# @app.route('/')
+
+# def flask():
+#     return "Hello" * 35
+# if __name__=="__main__":
+
+#     app.run(debug=True)
+
+# #99 dogs
+from flask import Flask
+app = Flask(__name__)
+
+print(__name__)
+@app.route('/')
+
+def flask():
+    return "Dogs" * 99
+if __name__=="__main__":
+    
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
